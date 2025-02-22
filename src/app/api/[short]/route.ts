a9da4f5cfb6ab5e08013e3cd395640b91574c813
Krishmika Abhishek <krishmikaabisheka1@gmail.com>
2025-02-23 03:23:56 +0530

    minor fixes

--- src/app/api/[short]/route.ts
+++ src/app/api/[short]/route.ts
@@ -3,7 +3,11 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function GET(req: Request, { params }: { params: { short: string } }) {
+interface Context {
+  params: { short: string };
+}
+
+export async function GET(req: Request, { params }: Context) {
   try {
     const urlEntry = await prisma.shortUrl.findUnique({
       where: { short: params.short }
@@ -14,7 +18,7 @@ export async function GET(req: Request, { params }: { params: { short: string } 
     } else {
       return NextResponse.json({ error: 'Short URL not found' }, { status: 404 });
     }
-  } catch {
+  } catch (error) {
     return NextResponse.json({ error: 'Server error' }, { status: 500 });
   }
 }
